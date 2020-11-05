85793fca41f1154f8c0ad2a5966aa7ad570ea81f
amodoipeter <amodoipeter@gmail.com>
2020-11-06 00:22:59 +0300

    created interfaces for department, news and the user

---
+++ src/main/java/Dao/DepartmentDao.java
@@ -0,0 +1,11 @@
+package Dao;
+
+public interface DepartmentDao {
+    List<Department> getAllDepartments();
+    List<User> getDepartmentUsersById(int id);
+    List<DepartmentNews> getDepartmentNewsById(int id);
+    void addDepartment(Department department);
+    Department findDepartmentById(int id);
+    void updateDepartment(Department department, String name, String description);
+    void clearAllDepartments();
+}
